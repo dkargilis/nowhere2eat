ca6c28912d9205749095f10e9623255c75316bdb
Daniel Kargilis <danielkargilis@Samanthas-MBP.hsd1.mi.comcast.net>
2020-05-10 01:09:37 -0400

    attempting to fix url

--- blog/static/blog/js/blog.js
+++ blog/static/blog/js/blog.js
@@ -840,7 +840,7 @@ function generateNewGroup(region){
     $.get('/newGroup.php', {}, function(json_response){
             urlCode = json_response.urlString.url
             titleInDatabase=urlCode
-            window.location.href = "http://localhost:8000/group/"+region+"/?t="+urlCode;
+            window.location.href = "https://shrouded-mesa-49267.herokuapp.com/group/"+region+"/?t="+urlCode;
     });
 
 
