cb06971c7805be944e1a1dea7b25752b94765cd2
DKargilis <danielkargilis@gmail.com>
2020-05-11 23:30:18 -0400

    allow xframe

--- django_project/settings.py
+++ django_project/settings.py
@@ -146,3 +146,7 @@ STATIC_URL = '/static/'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+X_FRAME_OPTIONS = 'ALLOWALL'
+
+XS_SHARING_ALLOWED_METHODS = ['POST','GET','OPTIONS', 'PUT', 'DELETE']
