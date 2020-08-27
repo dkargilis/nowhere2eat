254aebe1a5ffecacac51da3b89e5aab934322dff
DKargilis <danielkargilis@gmail.com>
2020-08-27 02:14:48 -0400

    test

--- blog/static/blog/js/blog.js
+++ blog/static/blog/js/blog.js
@@ -1009,7 +1009,7 @@ function updateClientFilters(){
 function generateNewGroup(region){
     //make ajax request that returns the title and url, set our title to titleindatabase
     //redirect to the generated url
-
+    alert("generating group")
     $.post('/newGroup.php', {}, function(json_response){
             urlCode = json_response.urlString.url
             titleInDatabase=urlCode
